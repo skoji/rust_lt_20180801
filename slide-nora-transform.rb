2dfaa811554a6aab58d2c675bacacd9175c8ebd6
Satoshi Kojima <skoji@mac.com>
2018-07-29 22:04:24 +0900

    fix indent

--- slide-nora-transform.rb
+++ slide-nora-transform.rb
@@ -1,35 +1,35 @@
 require 'rouge'
-  modify({:type => :HeadedSection}) do
-    @node.classes ||= []
-    @node.classes << 'page' if @node.level == 1
-    @node.classes << 'page' if @node.params[0]&.text == 'p'
-    @node.classes << 't' if @node.params[0]&.text == 't'
-    @node.classes << 'page' if @node.params[0]&.text == 't'
-    @node.classes << 'bookguide' if @node.params[0]&.text == 'bookguide'
-    @node.classes << 'license' if @node.params[0]&.text == 'license'
-  end
-  modify 'l' do
-    @node.add_attr 'target' => ['_blank']
-  end
-  replace ({type: :PreformattedBlock}) do
-    return @node if @node.name != 'code'
+modify({:type => :HeadedSection}) do
+  @node.classes ||= []
+  @node.classes << 'page' if @node.level == 1
+  @node.classes << 'page' if @node.params[0]&.text == 'p'
+  @node.classes << 't' if @node.params[0]&.text == 't'
+  @node.classes << 'page' if @node.params[0]&.text == 't'
+  @node.classes << 'bookguide' if @node.params[0]&.text == 'bookguide'
+  @node.classes << 'license' if @node.params[0]&.text == 'license'
+end
+modify 'l' do
+  @node.add_attr 'target' => ['_blank']
+end
+replace ({type: :PreformattedBlock}) do
+  return @node if @node.name != 'code'
 
-    case @node.codelanguage
-    when 'ruby'
-      lexer = Rouge::Lexers::Ruby.new
-    when 'rust'
-      lexer = Rouge::Lexers::Rust.new
-    when 'js'
-      lexer = Rouge::Lexers::Javascript.new
-    when 'c'
-      lexer = Rouge::Lexers::C.new
-    else
-      lexer = Rouge::Lexers::Shell.new
-    end
-    formatter = Rouge::Formatters::HTML.new
-    html = formatter.format(lexer.lex(@node.content.join "\r\n"))
-    text_node = text(html, raw_text: true)
-    text_node.noescape = true
-    code_node = block('code', text_node)
-    block('pre', code_node, classes: ['highlight'] )
+  case @node.codelanguage
+  when 'ruby'
+    lexer = Rouge::Lexers::Ruby.new
+  when 'rust'
+    lexer = Rouge::Lexers::Rust.new
+  when 'js'
+    lexer = Rouge::Lexers::Javascript.new
+  when 'c'
+    lexer = Rouge::Lexers::C.new
+  else
+    lexer = Rouge::Lexers::Shell.new
   end
+  formatter = Rouge::Formatters::HTML.new
+  html = formatter.format(lexer.lex(@node.content.join "\r\n"))
+  text_node = text(html, raw_text: true)
+  text_node.noescape = true
+  code_node = block('code', text_node)
+  block('pre', code_node, classes: ['highlight'] )
+end
